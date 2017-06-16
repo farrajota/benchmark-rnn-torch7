f04d0e31e7235e607dd79fe8c257c2d771348d77
farrajota <mglfarrajota@outlook.com>
2017-06-16 17:52:15 +0100

    Refactor text sampler

--- sample_chars.lua
+++ sample_chars.lua
@@ -39,7 +39,6 @@ torch.manualSeed(opt.manualSeed)
 -- Model
 --------------------------------------------------------------------------------
 
-opt.model = '/home/mf/Toolkits/Codigo/git/benchmark-rnn-torch7/data/exp/shakespear/length=50_lstm_cudnn_size=256_nlayers=2_dropout=0.00/checkpoint_1.t7'
 local checkpoint = torch.load(opt.model)
 local model = checkpoint[1]
 model:evaluate()
@@ -66,21 +65,15 @@ if string.len(opt.primetext) > 0 then
     print('seeding with ' .. seed_text)
     print('--------------------------')
     for c in seed_text:gmatch'.' do
-        prev_char = torch.Tensor{vocab[c]}
-        io.write(ivocab[prev_char[1]])
-        local lst = protos.rnn:forward{prev_char, unpack(current_state)}
-        -- lst is a list of [state1,state2,..stateN,output]. We want everything but last piece
-        current_state = {}
-        for i=1,state_size do
-            table.insert(current_state, lst[i])
-        end
-        prediction = lst[#lst] -- last element holds the log probabilities
+        next_char = torch.Tensor{vocab[c]}
+        io.write(ivocab[next_char[1]])
+        prediction = model:forward(next_char:view(1,1):cuda())
     end
 else
     -- fill with uniform probabilities over characters (? hmm)
     print('Seed text empty or missing, using uniform probability over first character.')
     print('--------------------------')
-    prediction = torch.Tensor(1, #ivocab):fill(1)/(#ivocab)
+    prediction = torch.DoubleTensor(1, #ivocab):fill(1)/(#ivocab)
 end
 
 -- start sampling/argmaxing
@@ -89,21 +82,21 @@ for i=1, opt.length do
     -- log probabilities from the previous timestep
     if opt.sample == 0 then
         -- use argmax
-        local _, prev_char_ = prediction:max(2)
-        prev_char = prev_char_:resize(1)
+        local _, next_char_ = prediction:max(2)
+        next_char = next_char_:resize(1)
     else
         -- use sampling
-        prediction:div(opt.temperature) -- scale by temperature
-        local probs = torch.exp(prediction):squeeze()
+        local probs = torch.div(prediction, opt.temperature):double():exp():squeeze()
         probs:div(torch.sum(probs)) -- renormalize so probs sum to one
-        prev_char = torch.multinomial(probs:float(), 1):resize(1):float()
+        next_char = torch.multinomial(probs, 1)
     end
 
     -- forward the rnn for next character
-    local lst = model:forward(prev_char:view(1,1))
-    prediction = lst:squeeze() --lst[#lst] -- last element holds the log probabilities
 
-    io.write(ivocab[prev_char[1]])
+    prediction = model:forward(next_char:view(1,1):cuda())
+    --prediction = lst --lst[#lst] -- last element holds the log probabilities
+
+    io.write(ivocab[next_char[1]])
 end
 io.write('\n')
 io.flush()
