a64bfd3199f9a40dfd3df2a4514071d9e5931b8c
farrajota <mglfarrajota@outlook.com>
2017-06-30 11:33:29 +0100

    Refactor train_all script

--- scripts/train_all.lua
+++ scripts/train_all.lua
@@ -9,18 +9,48 @@ local train_net = paths.dofile('train_rnn_network.lua')
 --[[ Load train configurations ]]--
 local configs = paths.dofile('train_configs.lua')
 
---[[ Train networks ]]--
-local networks_configs = {
+--[[ Networks ]]--
+local networks_configs = {  -- modelID = model_name_alias
     -- Vanilla
-    rnn_vanilla = train_net(configs, 'rnn-vanilla', 'rnn_vanilla'),
-    lstm_vanilla = train_net(configs, 'lstm-vanilla', 'lstm_vanilla'),
+    rnn_vanilla = 'rnn-vanilla',
+    lstm_vanilla = 'lstm-vanilla',
 
     -- rnn (Element-Research)
+    rnn_rnn = 'rnn-rnn',
+    lstm_rnn = 'lstm-rnn',
+    fastlstm_rnn = 'lstm(fast)-rnn',
+    gru_rnn = 'gru-rnn',
 
     -- rnnlib (facebook)
+    rnn_rnnlib = 'rnn-rnnlib',
+    lstm_rnnlib = 'lstm-rnnlib',
+    gru_rnnlib = 'gru-rnnlib',
 
     -- cudnn
+    rnnrelu_cudnn = 'rnn(relu)-cudnn',
+    rnntanh_cudnn = 'rnn(tanh)-cudnn',
+    lstm_cudnn = 'lstm-cudnn',
+    blstm_cudnn = 'blstm-cudnn',
+    gru_cudnn = 'gru-cudnn',
 }
 
---[[ Return the configurations for all networks ]]--
-return networks_configs
+local output = {}
+if g_skip_train then
+    --[[ Return the configurations for all networks ]]--
+    for k, v in pairs(networks_configs) do
+        local configs = paths.dofile('train_configs.lua')
+        configs.expID = v .. '-' .. g_hidden_dimension_size
+        configs.model = k
+        configs.dataset = g_dataset
+        configs.rnn_size = g_hidden_dimension_size
+        output[k] = configs
+    end
+else
+    --[[ Train networks ]]--
+    for k, v in pairs(networks_configs) do
+        output[k] = train_net(configs, v, k)
+    end
+end
+
+--[[ Return the configurations for all trained networks ]]--
+return output
